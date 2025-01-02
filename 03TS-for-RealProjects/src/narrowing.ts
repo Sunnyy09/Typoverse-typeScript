ce3e203c68155873958c1319f60540e8cd0c8c97
Sunnyy09 <12001109@csepup.ac.in>
2025-01-02 17:56:22 +0530

    explore more about narrowing in TS

--- 03TS-for-RealProjects/src/narrowing.ts
+++ 03TS-for-RealProjects/src/narrowing.ts
@@ -42,4 +42,70 @@ function logValue(x: Date | string){
     }
 }
 
-//using type predicates
+// ---- using type predicates
+
+type Fish =  {
+    swim:() => void
+}
+type Bird =  {
+    fly:() => void
+}
+
+function isFish(pet: Fish | Bird){
+    return (pet as Fish).swim !== undefined // return the boolean value
+}
+
+function getFood(pet : Fish | Bird){
+    if(isFish(pet)){
+        pet
+        return "fish food"
+    }else{
+        pet
+        return "bird food"
+    }
+}
+
+
+// Disciminated unions
+
+// interface Shape{
+//     type: "circle" | "rectangle"
+//     radius?: number
+//     sideLength?: number
+// }
+
+interface Circle {
+    kind: "circle"
+    radius: number
+}
+interface Square{
+    kind: "square"
+    side: number
+}
+interface Rectange {
+    kind: "rectangle"
+    length: number
+    width: number
+}
+
+type Shape = Circle | Square // for future purpose, you add Rectangle to it
+
+function getTrueShape(shape: Shape){
+    if(shape.kind === "circle"){
+        return Math.PI * shape.radius ** 2
+    }
+    return shape.side ** 2
+}
+
+function getArea(shape: Shape){
+    switch(shape.kind){
+        case "circle": 
+            return Math.PI * shape.radius ** 2 
+        case "square": 
+            return shape.side ** 2
+
+        default: 
+            const _defaultForShape: never = shape
+            return _defaultForShape
+    }
+}
